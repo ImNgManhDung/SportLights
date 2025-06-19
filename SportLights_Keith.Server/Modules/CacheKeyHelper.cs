8e0ef76bebc9181e0256cac6ea0c83b43db9e1a2
ImNMDung <nmdung181002@gmail.com>
2025-06-19 10:49:34 +0700

    Add CacheKeyHelper for entity-based cache key generation

---
+++ SportLights_Keith.Server/Modules/CacheKeyHelper.cs
@@ -0,0 +1,24 @@
+﻿using SPORTLIGHTS_SERVER.Areas.Admin.Controllers;
+
+namespace SPORTLIGHTS_SERVER.Modules
+{
+	public static class CacheKeyHelper
+	{
+		public static string Category(string searchValue, int page)
+			=> $"category:{searchValue}:{page}";
+
+		public static string Product(string searchValue, int page)
+			=> $"product:{searchValue}:{page}";
+		public static string Order(string searchValue, int page)
+			=> $"order:{searchValue}:{page}";
+
+		public static string Employee(string searchValue, int page)
+			=> $"employee:{searchValue}:{page}";
+		public static string Shipper(string searchValue, int page)
+			=> $"shipper:{searchValue}:{page}";
+
+		public static string Supplier(string searchValue, int page)
+			=> $"supplier:{searchValue}:{page}";
+
+	}
+}
