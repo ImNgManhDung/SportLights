2aed47849bf1f3839f1cb497a40520b68afc5c66
ImNMDung <nmdung181002@gmail.com>
2025-06-20 22:27:02 +0700

    Refactor extract IEmployeeRepository from EmployeeRepository
    Co-Authored-By: Nguyễn Mạnh Dũng <194352512+imngmanhdung@users.noreply.github.com>

---
+++ SportLights_Keith.Server/Areas/Admin/Repository/Employee/Abstractions/IEmployeeRepository.cs
@@ -0,0 +1,16 @@
+﻿using SPORTLIGHTS_SERVER.Areas.Admin.DTOs.Employees;
+using SPORTLIGHTS_SERVER.Entities;
+
+namespace SPORTLIGHTS_SERVER.Areas.Admin.Repository.Employees.Abstractions
+{
+	public interface IEmployeeRepository
+	{
+		int Count(EmployeeFilterDto filter);
+		Task<IReadOnlyList<Employee>> LoadEmployees(EmployeeFilterDto filter);
+		Task<Employee?> GetEmployeeById(int employeeId);
+		Task<int> CreateEmployee(CreateEmployeeDto dto);
+		Task<bool> UpdateEmployee(EditEmployeeDto dto);
+		Task<bool> DeleteEmployee(int employeeId);
+	}
+
+}
