a4a49471b0ad729aa2917303bdd837cdad4aeb88
ImNMDung <nmdung181002@gmail.com>
2025-06-24 15:35:13 +0700

    Add class EditCategoryDto

---
+++ SportLights_Keith.Server/Areas/Admin/DTOs/Categories/EditCategoryDto.cs
@@ -0,0 +1,18 @@
+﻿using SPORTLIGHTS_SERVER.Attributes.Validations;
+using SPORTLIGHTS_SERVER.Constants;
+using System.ComponentModel.DataAnnotations;
+
+namespace SPORTLIGHTS_SERVER.Areas.Admin.DTOs.Categories
+{
+	public class EditCategoryDto
+	{
+		public int CategoryId { get; set; }
+
+		[Display(Name = "Category Name")]
+		[Required(ErrorMessage = "{0} is required")]
+		[StringLength(ValidatesConstant.MAX_NAME, MinimumLength = ValidatesConstant.MIN_NAME,
+		ErrorMessage = "{0} must be at least {2} characters and at most {1} characters")]
+		public string? CategoryName { get; set; }
+		public string? CategoryDescription { get; set; }
+	}
+}
