1e20372560214474ad08eb1969ae8e9b1c04df3f
ImNMDung <nmdung181002@gmail.com>
2025-06-24 15:23:14 +0700

    Rename file IProductRepository

---
+++ SportLights_Keith.Server/Areas/Admin/Repository/Product/Abstractions/IProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using SPORTLIGHTS_SERVER.Areas.Admin.DTOs.Products;
+using SPORTLIGHTS_SERVER.Entities;
+
+namespace SPORTLIGHTS_SERVER.Areas.Admin.Repository.Products.Abstractions
+{
+	public interface IProductRepository
+	{
+		Task<int> CountProducts(ProductFilterDto filter, int categoryID = 0, int supplierID = 0, decimal minPrice = 0, decimal maxPrice = 0);
+
+		Task<int> CreateProduct(CreateProductDto dto);
+
+		Task<bool> DeleteProduct(int productId);
+
+		Task<Product?> GetProductById(int productId);
+
+		Task<IReadOnlyList<Product>> GetProducts(ProductFilterDto filter);
+
+		Task<bool> UpdateProduct(EditProductDto data);
+	}
+}
